19b20b87f23217801a37d3112c9e4bb56f2a46a7
Son Nguyen <son@spiderbox.design>
2019-09-09 18:29:35 +0700

    Added gcb ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import kebabcase from "lodash.kebabcase";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    featureBranch: "feature/",
+    hotfixBranch: "hotfix/",
+    bugBranch: "bug/",
+    releaseBranch: "release/"
+  };
+
+  onBranchNameInputChange = e => {
+    const text = e.target.value;
+    const kebabcase_text = kebabcase(text);
+
+    this.setState({
+      featureBranch: `feature/${kebabcase_text}`,
+      hotfixBranch: `hotfix/${kebabcase_text}`,
+      bugBranch: `bug/${kebabcase_text}`,
+      releaseBranch: `release/${kebabcase_text}`
+    });
+  };
+
+  gitComand = text => {
+    return `git checkout -b ${text}`;
+  };
+
+  render() {
+    const {
+      featureBranch,
+      hotfixBranch,
+      bugBranch,
+      releaseBranch
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>git checkout -b</h1>
+
+        <div className="input-container">
+          <input
+            className="input-field input-branch-in"
+            placeholder="type here"
+            onChange={e => this.onBranchNameInputChange(e)}
+          />
+        </div>
+
+        <div className="input-container">
+          <input
+            className="input-field input-branch-out"
+            value={featureBranch}
+            onChange={e => this.setState({ featureBranch: e.target.value })}
+          />
+          <div className="btn--inside">
+            <CopyToClipboard text={featureBranch}>
+              <button className="btn btn--primary">B</button>
+            </CopyToClipboard>
+            <CopyToClipboard text={this.gitComand(featureBranch)}>
+              <button className="btn btn--primary ml-1">G</button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <div className="input-container">
+          <input
+            className="input-field input-branch-out"
+            value={hotfixBranch}
+            onChange={e => this.setState({ hotfixBranch: e.target.value })}
+          />
+          <div className="btn--inside">
+            <CopyToClipboard text={hotfixBranch}>
+              <button className="btn btn--primary">B</button>
+            </CopyToClipboard>
+            <CopyToClipboard text={this.gitComand(hotfixBranch)}>
+              <button className="btn btn--primary ml-1">G</button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <div className="input-container">
+          <input
+            className="input-field input-branch-out"
+            value={bugBranch}
+            onChange={e => this.setState({ bugBranch: e.target.value })}
+          />
+          <div className="btn--inside">
+            <CopyToClipboard text={bugBranch}>
+              <button className="btn btn--primary">B</button>
+            </CopyToClipboard>
+            <CopyToClipboard text={this.gitComand(bugBranch)}>
+              <button className="btn btn--primary ml-1">G</button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <div className="input-container">
+          <input
+            className="input-field input-branch-out"
+            value={releaseBranch}
+            onChange={e => this.setState({ releaseBranch: e.target.value })}
+          />
+          <div className="btn--inside">
+            <CopyToClipboard text={releaseBranch}>
+              <button className="btn btn--primary">B</button>
+            </CopyToClipboard>
+            <CopyToClipboard text={this.gitComand(releaseBranch)}>
+              <button className="btn btn--primary ml-1">G</button>
+            </CopyToClipboard>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
